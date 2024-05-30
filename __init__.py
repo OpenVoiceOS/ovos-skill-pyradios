35eb678be388a10c30ce0c5ac8a2236c8973729f
Flávio Eler De Melo <flavio.eler@gmail.com>
2024-05-30 10:55:48 +0200

    Add caching mechanism

--- __init__.py
+++ __init__.py
@@ -1,4 +1,4 @@
-from os.path import join, dirname
+import os
 from typing import Generator
 
 from ovos_utils.ocp import MediaType, PlaybackType
@@ -6,21 +6,68 @@ from ovos_utils.log import LOG
 from ovos_workshop.decorators import ocp_search
 from ovos_workshop.skills.common_play import OVOSCommonPlaybackSkill
 from pyradios import RadioBrowser
+from dead_simple_cache import SimpleCache
 from rapidfuzz.distance import DamerauLevenshtein
+
+
+BASE_DIR = os.getenv("HOME") or os.path.dirname(os.path.abspath(__file__))
+DEFAULT_CACHE_PATH = os.path.join(BASE_DIR, ".cache", "pyradios")
 
 
 class PyradiosSkill(OVOSCommonPlaybackSkill):
     def __init__(self, *args, **kwargs):
         super().__init__(supported_media = [MediaType.RADIO],
-                         skill_icon=join(dirname(__file__), "res", "radio-tuner.png"),
+                         skill_icon=os.path.join(os.path.dirname(__file__), "res", "radio-tuner.png"),
                          *args, **kwargs)
+        self.cache = SimpleCache(file_path=DEFAULT_CACHE_PATH, open=True)
         self.radio_browser = RadioBrowser()
+
+    def __del__(self):
+        # Makes cache persistent on disk
+        self.cache.close()
+
+    def search_cache(self, query: str) -> list:
+        """Search for cached stations."""
+        items = self.cache.get(query, fuzzy=True)
+        server_alive = {}
+        for key, stations in items.items():
+            for station in stations:
+                url = station["url"]
+                # Check whether each server is alive
+                if url not in server_alive:
+                    # TODO: find a faster way to check whether each server is up
+                    # response = requests.head(url, timeout=1)
+                    # code = response.status_code
+                    # server_alive[url] = str(code).startswith('2') or str(code).startswith('3')
+                    server_alive[url] = True
+                if not server_alive[url]:
+                    stations.remove(station)
+        for key, stations in items.items():
+            if stations:
+                self.cache.replace(key=key, data=stations)
+            else:
+                self.cache.delete(key=key)
+        return sum(list(items.values()), [])
+
+    def search(self, query: str) -> list:
+        """General search method."""
+        # Search for cached items
+        cached_items = self.search_cache(query)
+        if cached_items:
+            stations = cached_items
+        else:
+            # Search again
+            stations = self.radio_browser.search(name=query, hidebroken=True)
+            # Update cache
+            for station in filter(lambda s: s["name"] != '', stations):
+                self.cache.add(key=query, data=station)
+        return stations
 
     @ocp_search()
     def search_pyradios(self, phrase: str, media_type: MediaType) -> Generator[dict, None, None]:
         """Search radio with Pyradios."""
         base_score = 0
-        
+
         if media_type == MediaType.RADIO or self.voc_match(phrase, "radio"):
             base_score += 30
         else:
@@ -36,7 +83,7 @@ class PyradiosSkill(OVOSCommonPlaybackSkill):
             queries.append(phrase_without_radio)
         queries.append(phrase)
         for query in queries:
-            for ch in self.radio_browser.search(name=query):
+            for ch in self.search(query=query):
                 score = base_score + int(DamerauLevenshtein.normalized_similarity(ch["name"], query) * 80)
                 LOG.debug(f"Query: {query}, match: {ch['name']}, score: {score}")
                 yield {
